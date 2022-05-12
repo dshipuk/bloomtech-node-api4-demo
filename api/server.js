7d3946d8652404bf3329a00af67822ca3e0ab4cf
unknown <dshipuk@yahoo.com>
2022-05-12 10:14:51 -0700

    Added base link

--- api/server.js
+++ api/server.js
@@ -2,6 +2,10 @@ const express = require("express");
 
 const server = express();
 
+server.get("/", (req,res) => {
+    res.send("<h1>Heroku Deployment</h1>");
+})
+
 server.get("/node-env", (req,res) => {
     res.json(process.env.NODE_ENV);
 })
